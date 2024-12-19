1c06a9b6ee82d51bb102cde3fe64fc2168411fa8
Christopher Hampel <christopher.hampel@yaho.de>
2024-12-19 11:57:26 +0100

    add routerlink in login.html

--- src/app/registration/login/login.component.ts
+++ src/app/registration/login/login.component.ts
@@ -2,11 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [ ReactiveFormsModule, CommonModule ],
+  imports: [ ReactiveFormsModule, CommonModule, RouterLink ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
