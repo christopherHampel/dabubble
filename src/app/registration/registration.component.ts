4ac016a8ac8871bbfbcbf5f90c6277db48b9b87d
Stephan Schmidt <81927898+Spawn821@users.noreply.github.com>
2024-12-19 21:34:01 +0100

    registration style

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [
+    LoginComponent
+  ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
