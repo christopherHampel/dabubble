a2d1d1af7cf354b3561dfc04152e2e0f1607e2a4
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2024-12-21 09:37:30 +0100

    start-animation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { interval, map, take } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-registration',
   imports: [
-    RouterOutlet, RouterLink
+    RouterOutlet, RouterLink, AsyncPipe
   ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss',
@@ -12,4 +14,14 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 })
 export class RegistrationComponent {
 
+  typedText$ = this.getTypewriterEffect('DABubble', 200).pipe(
+    map((text) => text)
+  );
+
+  getTypewriterEffect(word: string, speed: number = 200) {
+    return interval(speed).pipe(
+      map((x) => word.substring(0, x + 1)),
+      take(word.length)
+    );
+  }
 }
