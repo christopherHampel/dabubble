4cb8a022e4cabe63632a38c88aefa74be3799bad
Stephan Schmidt <81927898+Spawn821@users.noreply.github.com>
2024-12-27 18:00:41 +0100

    if clicked friend dropdown close and inputfield changed with friend view

--- src/app/chatroom/devspace/devspace-directmessages/add-friend-dialog/add-friend-dialog.component.ts
+++ src/app/chatroom/devspace/devspace-directmessages/add-friend-dialog/add-friend-dialog.component.ts
@@ -4,6 +4,7 @@ import { DevspaceDirectmessagesComponent } from '../devspace-directmessages.comp
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { UsersDbService } from '../../../../services/usersDb/users-db.service';
+import { UserProfile } from 'firebase/auth';
 
 @Component({
   selector: 'app-add-friend-dialog',
@@ -18,20 +19,40 @@ export class AddFriendDialogComponent {
   readonly dialogRef = inject(MatDialogRef<DevspaceDirectmessagesComponent>);
   usersDb = inject(UsersDbService)
   userName: string = '';
-  users: string[] = [
-    'Christopher',
-    'Stephan',
-    'Theo',
-    'Sabine',
-    'Helga'
-  ]
+  userExistList: UserProfile[] = [{
+    id: '',
+    userName: '',
+    email: '',
+    avatar: '',
+    active: false
+  }];
+  selectedUser: UserProfile = {};
 
   getUserList() {
-    console.log(this.usersDb.currentUserSig());
     return this.usersDb.userListSig();
   }
 
-  filterUserName(currentUser: string): boolean {
+  removeUser() {
+    this.selectedUser = {};
+  }
+
+  selectUser(id: string | undefined) {
+    const user: any = this.getUserList().find(user => user.id == id);
+    this.selectedUser = {
+      id: user.id,
+      userName: user.userName,
+      email: user.emai,
+      avatar: user.avatar,
+      active: user.active
+    }
+    this.userName = '';
+  }
+
+  filteredUsersAfterInput() {
+    return this.getUserList().filter(() => this.filterUserExist(this.userName));
+  }
+
+  filterUserExist(currentUser: string): boolean {
     if (!this.userName) {
       return false;
     } else {
