0a8e85654e15ee0e59afcfe913b1784ff5f51c6d
Stephan Schmidt <81927898+Spawn821@users.noreply.github.com>
2025-01-26 22:22:31 +0100

    50% of the same area for channels and directmessags in devspace

--- src/app/chatroom/devspace/devspace-channels/devspace-channels.component.ts
+++ src/app/chatroom/devspace/devspace-channels/devspace-channels.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-devspace-channels',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './devspace-channels.component.html',
   styleUrl: './devspace-channels.component.scss'
 })
 export class DevspaceChannelsComponent {
+  channelsOpen: boolean = false;
 
+  openChannels() {
+    if (this.channelsOpen) {
+      this.channelsOpen = false;
+    } else {
+      this.channelsOpen = true;
+    }
+  }
 }
