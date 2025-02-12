ebcf267ce4e825f0699339ae4267887214111f84
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-02-12 19:15:27 +0100

    change text own-chat

--- src/app/chatroom/messages/direct-message/direct-message.component.ts
+++ src/app/chatroom/messages/direct-message/direct-message.component.ts
@@ -5,20 +5,22 @@ import { FormsModule } from '@angular/forms';
 import { ChatsService } from '../../../services/message/chats.service';
 import { TextareaComponent } from '../../../shared/textarea/textarea.component';
 import { SingleMessageComponent } from '../../messages/single-message/single-message.component';
-import { async, Observable, Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { EmojiPickerComponentComponent } from '../../../shared/textarea/emoji-picker-component/emoji-picker-component.component';
 import { EmojisService } from '../../../services/message/emojis.service';
 import { ScrollService } from '../../../services/message/scroll.service';
 import { AuthService } from '../../../services/auth/auth.service';
 import { UsersDbService } from '../../../services/usersDb/users-db.service';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-direct-message',
   imports: [ CommonModule, FormsModule, TextareaComponent, SingleMessageComponent, EmojiPickerComponentComponent ],
   templateUrl: './direct-message.component.html',
   styleUrl: './direct-message.component.scss',
-  providers: [ScrollService] // Eigene Instanz des Services
+  providers: [ScrollService]
 })
+
 export class DirectMessageComponent implements OnInit, OnDestroy, OnChanges {
 
   @ViewChild('myScrollContainer') private myScrollContainer!: ElementRef;
@@ -31,12 +33,13 @@ export class DirectMessageComponent implements OnInit, OnDestroy, OnChanges {
   emojiService = inject(EmojisService);
   private logoutSubscription!: Subscription;
   private paramMapSubscription!: Subscription;
-
+  
   constructor(  private route: ActivatedRoute, 
                 public chatService: ChatsService,
                 private scrollService: ScrollService,
                 private authService: AuthService,
-                private usersService: UsersDbService) { }
+                private usersService: UsersDbService,
+                private sanitizer: DomSanitizer) { }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['chatId']) {
@@ -112,11 +115,15 @@ export class DirectMessageComponent implements OnInit, OnDestroy, OnChanges {
     return '(chatMessages$ | async)?.length === 0';
   }
 
-  getStartText() {
-    if(this.chatService.chatPartner.name == this.usersService.currentUserSig()?.userName) {
-      return 'Hau rein digga'
+  getStartText(): SafeHtml {
+    if (this.chatService.chatPartner.name === this.usersService.currentUserSig()?.userName) {
+      return this.sanitizer.bypassSecurityTrustHtml(
+        '<span class="bold-font">Dieser Raum ist nur für dich da.</span> Mache dir Notizen, liste deine To-dos auf oder bewahre Links und Dateien griffbereit auf. Du kannst hier auch gerne Dinge mit dir selbst besprechen.'
+      );
     } else {
-      return `Diese Unterhaltung findet nur zwischen @${this.chatService.chatPartner.name} und dir statt.`
+      return this.sanitizer.bypassSecurityTrustHtml(
+        `Diese Unterhaltung findet nur zwischen @${this.chatService.chatPartner.name} und dir statt.`
+      );
     }
   }
 }
