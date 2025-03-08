6290be857c7fa95a2d30db494cea25ef1130644b
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-03-08 10:12:17 +0100

    start with serach chat function at default

--- src/app/chatroom/messages/default/default.component.ts
+++ src/app/chatroom/messages/default/default.component.ts
@@ -1,17 +1,26 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TextareaComponent } from '../../../shared/textarea/textarea.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UserViewSmallComponent } from "../../../shared/user-view-small/user-view-small.component";
+import { UsersDbService } from '../../../services/usersDb/users-db.service';
+
 
 @Component({
   selector: 'app-default',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [ TextareaComponent, CommonModule],
+  imports: [TextareaComponent, CommonModule, FormsModule, UserViewSmallComponent],
   templateUrl: './default.component.html',
   styleUrl: './default.component.scss',
 })
 export class DefaultComponent {
 
   isLoaded = false;
+  userList: boolean = false;
+  searchText:string ='';
+  filteredUser: any[] = [];
+
+  constructor(private userService: UsersDbService) {}
 
   ngOnInit() {
     setTimeout(() => {
@@ -19,4 +28,23 @@ export class DefaultComponent {
     }, 50);
   }
 
+  searchChat() {
+    if (this.searchText == '#') {
+        console.log('Piep');
+    } else if (this.searchText.startsWith('@')) {
+        this.userList = true;
+        this.searchUserList();
+        console.log(this.filteredUser);
+    } else {
+        this.userList = false;
+    }
+}
+
+searchUserList() {
+  const query = this.searchText.substring(1); 
+
+  this.filteredUser = this.userService.userList.filter(user => 
+      user.userName.includes(query)
+  );
+}
 }
