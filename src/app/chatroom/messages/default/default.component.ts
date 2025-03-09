f504d11a622ee48f29f530cec10d42d70f580ff9
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-03-09 07:27:27 +0100

    search private chats

--- src/app/chatroom/messages/default/default.component.ts
+++ src/app/chatroom/messages/default/default.component.ts
@@ -2,25 +2,36 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TextareaComponent } from '../../../shared/textarea/textarea.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { UserViewSmallComponent } from "../../../shared/user-view-small/user-view-small.component";
+import { UserViewSmallComponent } from '../../../shared/user-view-small/user-view-small.component';
 import { UsersDbService } from '../../../services/usersDb/users-db.service';
-
+import { UserProfile } from 'firebase/auth';
+import { ChatsService } from '../../../services/message/chats.service';
+import { Router } from '@angular/router';
+import { User } from '../../../interfaces/user';
 
 @Component({
   selector: 'app-default',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [TextareaComponent, CommonModule, FormsModule, UserViewSmallComponent],
+  imports: [
+    TextareaComponent,
+    CommonModule,
+    FormsModule,
+    UserViewSmallComponent,
+  ],
   templateUrl: './default.component.html',
   styleUrl: './default.component.scss',
 })
 export class DefaultComponent {
-
   isLoaded = false;
   userList: boolean = false;
-  searchText:string ='';
+  searchText: string = '';
   filteredUser: any[] = [];
 
-  constructor(private userService: UsersDbService) {}
+  constructor(
+    private userService: UsersDbService,
+    private chatService: ChatsService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     setTimeout(() => {
@@ -30,21 +41,34 @@ export class DefaultComponent {
 
   searchChat() {
     if (this.searchText == '#') {
-        console.log('Piep');
+      console.log('Piep');
     } else if (this.searchText.startsWith('@')) {
-        this.userList = true;
-        this.searchUserList();
-        console.log(this.filteredUser);
+      this.userList = true;
+      this.searchUserList();
+      console.log(this.filteredUser);
     } else {
-        this.userList = false;
+      this.userList = false;
     }
-}
+  }
 
-searchUserList() {
-  const query = this.searchText.substring(1); 
+  searchUserList() {
+    const query = this.searchText.substring(1);
 
-  this.filteredUser = this.userService.userList.filter(user => 
+    this.filteredUser = this.userService.userList.filter((user) =>
       user.userName.includes(query)
-  );
-}
+    );
+  }
+
+  async selectChat(user: any) {
+    try {
+      const chatId = await this.chatService.setPrivateChat(user, 'messages');
+      this.chatService.currentChatId = chatId;
+      this.router.navigate([
+        '/chatroom',
+        { outlets: { chats: ['direct-message', chatId], thread: null } },
+      ]);
+    } catch (error) {
+      console.error('Fehler beim Erstellen des Chats:', error);
+    }
+  }
 }
