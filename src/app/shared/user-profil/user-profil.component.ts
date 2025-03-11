c76162ec851109992c2ab9d919b3d04cdbc0353f
Stephan Schmidt <81927898+Spawn821@users.noreply.github.com>
2025-03-11 21:27:48 +0100

    avatars in a circle around the current avatar

--- src/app/shared/user-profil/user-profil.component.ts
+++ src/app/shared/user-profil/user-profil.component.ts
@@ -11,4 +11,30 @@ import { CommonModule } from '@angular/common';
 })
 export class UserProfilComponent {
   @Input() dialog: boolean = false;
+
+  images = [
+    '/img/elias_neumann.png',
+    '/img/elise_roth.png',
+    '/img/frederik_beck.png',
+    '/img/noah_braun.png',
+    '/img/sofia_müller.png',
+    '/img/steffen_hoffmann.png'
+  ]
+
+  selectedImage = this.images[0];
+
+  selectImage(img: string) {
+    this.selectedImage = img;
+  }
+
+  getPosition(index: number) {
+    const angle = (index / this.images.length) * 2 * Math.PI;
+    const radius = 130;
+    const x = radius * Math.cos(angle);
+    const y = radius * Math.sin(angle);
+    return {
+      top: `${x}px`,
+      left:`${y}px`
+    };
+  }
 }
